61041b66f547a7a8d15a2ce5a8432dbecf59c3a8
Ryan <hashtagpoop@gmail.com>
2021-10-08 20:36:06 -0700

    ./static/assets

--- static/components/add_recipe_modal.js
+++ static/components/add_recipe_modal.js
@@ -1,9 +1,29 @@
+// Edits here should be checked against edit_recipe_modal since the files operate similarily
+
 var AddRecipeModal = {
 
     props: [
     ],
     data() {
         return {
+            types: ["Breakfast", "Lunch", "Dinner", "Side", "Dessert"],
+            users: ["M&G", "Karen"],
+            // Title: "",
+            // Ingredients: "",
+            // Instructions: "",
+            // TotalTime: "",
+            // Serves: "",
+            // Tags: "",
+            // ImageSrc: "",
+            Title: "Bitez Burger",
+            Ingredients: "",
+            Instructions: "",
+            TotalTime: "20min",
+            Serves: 6,
+            Tags: "",
+            ImageSrc: "https://bitezburger.com/static/images/burger-closeup.jpg",
+            Type: null,
+            User: null,
         }
     },
     methods: {
@@ -11,8 +31,71 @@ var AddRecipeModal = {
             this.$emit('close')
         },
         submitRecipe: function() {
+            this.splitIngredients();
+            this.splitInstructions();
+            this.splitTags();
 
-        }
+            var newRecipe = { 
+                Title: this.Title,
+                Ingredients: this.Ingredients,
+                Instructions: this.Instructions, 
+                TotalTime: this.TotalTime,
+                Serves: this.Serves,
+                Tags: this.Tags,
+                ImageSrc: this.ImageSrc,
+                Type: this.Type || this.types[0],
+                User: this.User || this.users[0],
+            }
+
+            console.log(newRecipe);
+            this.$emit("submit", newRecipe)            
+            this.hideModal();
+        },
+        selectedType: function(event) {
+            this.Type = event.target.value;
+        },
+        selectedUser: function(event) {
+            this.User = event.target.value;
+        },
+        splitTags: function() {
+            if (this.Tags != "") {
+                if (this.Tags.search("\n") > -1) {
+                    var delimeterRegex = /,*\n/;
+                } else {
+                    var delimeterRegex = /,\n*/;
+                }
+                var newListOfTags = this.Tags.split(delimeterRegex);
+                this.Tags = newListOfTags;
+            } else {
+                console.log("No tags added");
+            }
+        },
+        splitIngredients: function() {
+            if (this.Ingredients.search("\n") > -1) {
+                var delimeterRegex = /,*\n/;
+            } else {
+                var delimeterRegex = /,\n*/;
+            }
+            var newListOfIngredients = this.Ingredients.split(delimeterRegex);
+            newListOfIngredients = newListOfIngredients.filter((currentElement) => {
+                return currentElement.trim().length > 0;
+            });
+            newListOfIngredients = newListOfIngredients.map(string => string.trim());
+            this.Ingredients = newListOfIngredients;
+        },
+        splitInstructions: function() {
+            const regex = /([\(\s\-]*\d+[\)]*)/g;
+            var newListOfText = [];
+            var text = this.Instructions;
+
+            newListOfInstructions = text.replace(regex, "\n$1").split(/,*\n+/)
+            newListOfInstructions = newListOfInstructions.filter((currentElement) => {
+                return currentElement.trim().length > 0;
+            });
+            newListOfInstructions = newListOfInstructions.map(string => string.trim());
+
+            this.Instructions = newListOfInstructions;
+        },
     },
     template:   `
         <div class="addFormContainer">
@@ -22,63 +105,63 @@ var AddRecipeModal = {
 
             <form action="#" @submit.prevent="submitRecipe">
                 <div class="name">
-                <label for="Title">Title</label>
-                <input type="text" autocomplete="off" placeholder="e.g. Creamed Corn" name="Title" required>
+                    <label for="Title">Title</label>
+                    <input v-model="Title" type="text" autocomplete="off" placeholder="e.g. Creamed Corn" name="Title" required>
                 </div>
 
                 <div class="two-grid">
                     <div class="two-grid">
                         <div>
                             <label for="TotalTime">Total Time</label>
-                            <input type="text" autocomplete="off" placeholder="e.g. 50min" name="TotalTime" required>
+                            <input v-model="TotalTime" type="text" autocomplete="off" placeholder="e.g. 50min" name="TotalTime" required>
                         </div>
 
                         <div>
                             <label for="Servings">Serves</label>
-                            <input type="number" placeholder="e.g. 6" name="Servings" required>
+                            <input v-model="Serves" type="number" placeholder="e.g. 6" name="Servings" required>
                         </div>
                     </div>
 
                     <div class="two-grid">
                         <div>
                             <label for="Type">Type</label>
-                            <select name="Type" required>
-                                <option>Breakfast</option>
-                                <option>Lunch</option>
-                                <option>Dinner</option>
-                                <option>Side</option>
-                                <option>Dessert</option>
+                            <select @change="selectedType" name="Type" required>
+                                <option v-for="type in types">{{ type }}</option>
                             </select>
                         </div>
 
                         <div>
                             <label for="User">User</label>
-                            <select name="User" required>
-                                <option>M&G</option>
-                                <option>Karen</option>
+                            <select @change="selectedUser" name="User" required>
+                                <option v-for="user in users">{{ user }}</option>
                             </select>
                         </div>
                     </div>
                 </div>
 
                 <div>
-                <label for="name">Tags</label>
-                <input type="text" autocomplete="off" placeholder="Italian, Fast, Leftovers" name="Tags">
+                    <label for="name">Tags</label>
+                    <input type="text" v-model="Tags" autocomplete="off" placeholder="Italian, Fast, Leftovers" name="Tags">
                 </div>
 
                 <div>
-                <label for="ImageURL">Image URL</label>
-                <input type="text" autocomplete="off" placeholder="to insert url image" name="ImageURL" required>
+                    <label for="ImageURL">Image URL</label>
+                    <input type="text" v-model="ImageSrc" autocomplete="off" placeholder="to insert url image" name="ImageURL" required>
                 </div>
 
                 <div>
-                <label for="Ingredients">Ingredients</label>
-                <textarea name="Ingredients" placeholder="1. Secret sauce" cols="30" rows="5" required></textarea>
+                    <label for="Ingredients">
+                        Ingredients
+                        <div class="tooltip">
+                            <p class="tooltip-text" tooltip-text="You can list out ingredients separated by commas or by hitting enter!">?</p>
+                        </div>
+                    </label>
+                    <textarea v-model="Ingredients" name="Ingredients" placeholder="Secret sauce, Spinach" cols="30" rows="5"></textarea>
                 </div>
 
                 <div>
-                <label for="Instructions">Instructions</label>
-                <textarea name="Instructions" placeholder="1. Cut the tomatoes" cols="30" rows="5" required></textarea>
+                    <label for="Instructions">Instructions</label>
+                    <textarea v-model="Instructions" name="Instructions" placeholder="1. Cut the tomatoes" cols="30" rows="5"></textarea>
                 </div>
 
                 <div class="two-grid">
--- static/components/edit_recipe_modal.js
+++ static/components/edit_recipe_modal.js
@@ -1,3 +1,5 @@
+// Edits here should be checked against add_recipe_modal since the files operate similarily
+
 var EditRecipeModal = {
 
     props: [
@@ -7,15 +9,81 @@ var EditRecipeModal = {
         return {
             types: ["Breakfast", "Lunch", "Dinner", "Side", "Dessert"],
             users: ["M&G", "Karen"],
+            editedIngredients: false,
+            editedInstructions: false,
+            Index: this.recipe.Index,
+            Title: this.recipe.Title,
+            Ingredients: this.recipe.Ingredients,
+            Instructions: this.recipe.Instructions,
+            TotalTime: this.recipe.TotalTime,
+            Serves: this.recipe.Serves,
+            Tags: this.recipe.Tags,
+            ImageSrc: this.recipe.ImageSrc,
+            Type: this.recipe.Type,
+            User: this.recipe.User,
         }
     },
     methods: {
         hideModal: function() {
             this.$emit('close')
         },
-        submitRecipe: function() {
+        submitUpdate: function() {
+            if (this.editedIngredients) {
+                this.splitIngredients();
+            };
+            if (this.editedInstructions) {
+                this.splitInstructions();
+            };
 
-        }
+            var editedRecipe = { 
+                Index: this.Index,
+                Title: this.Title,
+                Ingredients: this.Ingredients,
+                Instructions: this.Instructions, 
+                TotalTime: this.TotalTime,
+                Serves: this.Serves,
+                Tags: this.Tags,
+                ImageSrc: this.ImageSrc,
+                Type: this.Type || this.types[0],
+                User: this.User || this.users[0],
+            };
+
+            console.log(editedRecipe);
+            this.$emit("submit", editedRecipe)            
+            this.hideModal();
+        },
+        selectedType: function(event) {
+            this.Type = event.target.value;
+        },
+        selectedUser: function(event) {
+            this.User = event.target.value;
+        },
+        splitIngredients: function() {
+            if (this.Ingredients.search("\n") > -1) {
+                var delimeterRegex = /,*\n/;
+            } else {
+                var delimeterRegex = /,\n*/;
+            }
+            var newListOfIngredients = this.Ingredients.split(delimeterRegex);
+            newListOfIngredients = newListOfIngredients.filter((currentElement) => {
+                return currentElement.trim().length > 0;
+            });
+            newListOfIngredients = newListOfIngredients.map(string => string.trim());
+            this.Ingredients = newListOfIngredients;
+        },
+        splitInstructions: function() {
+            const regex = /([\(\s\-]*\d+[\)]*)/g;
+            var newListOfText = [];
+            var text = this.Instructions;
+
+            newListOfInstructions = text.replace(regex, "\n$1").split(/,*\n+/)
+            newListOfInstructions = newListOfInstructions.filter((currentElement) => {
+                return currentElement.trim().length > 0;
+            });
+            newListOfInstructions = newListOfInstructions.map(string => string.trim());
+
+            this.Instructions = newListOfText;
+        },
     },
     template:   `
         <div class="editFormContainer">
@@ -23,64 +91,66 @@ var EditRecipeModal = {
 
             <br>
 
-            <form action="#" @submit.prevent="submitRecipe">
+            <form action="#" @submit.prevent="submitUpdate">
                 <div class="name">
-                <label for="Title">Title</label>
-                <input type="text" autocomplete="off" :value="recipe.Title" name="Title" required>
+                    <label for="Title">Title</label>
+                    <input type="text" autocomplete="off" v-model="Title" name="Title" required>
                 </div>
 
                 <div class="two-grid">
                     <div class="two-grid">
                         <div>
                             <label for="TotalTime">Total Time</label>
-                            <input type="text" autocomplete="off" :value="recipe.TotalTime" name="TotalTime" required>
+                            <input type="text" autocomplete="off" v-model="TotalTime" name="TotalTime" required>
                         </div>
 
                         <div>
                             <label for="Servings">Serves</label>
-                            <input type="number" :value="recipe.Serves" name="Servings" required>
+                            <input type="number" v-model="Serves" name="Servings" required>
                         </div>
                     </div>
 
                     <div class="two-grid">
                         <div>
                             <label for="Type">Type</label>
-                            <select name="Type" required>
-                                <option v-for="type in types" :selected="type == recipe.Type">{{ type }}</option>
+                            <select @change="selectedType" name="Type" required>
+                                <option v-for="type in types" :selected="type == Type">{{ type }}</option>
                             </select>
                         </div>
 
                         <div>
                             <label for="User">User</label>
-                            <select name="User" required>
-                                <option v-for="user in users" :selected="user == recipe.User">{{ user }}</option>
+                            <select @change="selectedUser" name="User" required>
+                                <option v-for="user in users" :selected="user == User">{{ user }}</option>
                             </select>
                         </div>
                     </div>
                 </div>
 
                 <div>
-                <label for="name">Tags</label>
-                <input type="text" autocomplete="off" :value="recipe.Tags" name="Tags">
+                    <label for="name">Tags</label>
+                    <input type="text" autocomplete="off" v-model="Tags" name="Tags">
                 </div>
 
                 <div>
-                <label for="ImageURL">Image URL</label>
-                <input type="text" autocomplete="off" :value="recipe.ImageSrc" name="ImageURL" required>
+                    <label for="ImageURL">Image URL</label>
+                    <input type="text" autocomplete="off" :value="ImageSrc" name="ImageURL" required>
                 </div>
 
                 <div>
-                <label for="Ingredients">Ingredients</label>
-                <textarea name="Ingredients" :value="recipe.Ingredients" cols="30" rows="5" required></textarea>
+                    <label for="Ingredients">Ingredients</label>
+                    <textarea v-if="typeof(Ingredients) == 'string'" @change="editedIngredients=true" name="Ingredients" v-model="Ingredients" cols="30" rows="5"></textarea>
+                    <textarea v-else @change="editedIngredients=true; Ingredients = $event.target.value" name="Ingredients" :value="Ingredients.join('\\n')" cols="30" rows="5"></textarea>
                 </div>
 
                 <div>
-                <label for="Instructions">Instructions</label>
-                <textarea name="Instructions" :value="recipe.Instructions" cols="30" rows="5" required></textarea>
+                    <label for="Instructions">Instructions</label>
+                    <textarea v-if="typeof(Instructions) == 'string'" @change="editedInstructions=true" name="Instructions" v-model="Instructions" cols="30" rows="5"></textarea>
+                    <textarea v-else @change="editedInstructions=true; Instructions = $event.target.value" :value="Instructions.join('\\n')" cols="30" rows="5"></textarea>
                 </div>
 
                 <div style="display:grid; grid-template-columns: 1fr 1fr 1fr;column-gap: 30px;">
-                    <input type="submit" value="Add Recipe" class="add_recipe_button" />
+                    <input type="submit" value="Update Recipe" class="add_recipe_button" />
                     <input type="submit" value="Delete Recipe" class="delete_recipe_button" />
                     <input @click="hideModal" type="button" value="Close" class="close_button" />
                 </div>
