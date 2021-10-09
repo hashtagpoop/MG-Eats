6138396224975efe1d32ad0533b22fc8b2add293
Ryan <hashtagpoop@gmail.com>
2021-10-08 20:36:06 -0700

    nearly done with front end modals and forms

--- static/components/view_recipe_modal.js
+++ static/components/view_recipe_modal.js
@@ -18,14 +18,16 @@ var RecipeModal = {
     
             <div class="recipe-info" style="color: #F7E1D7;font-size: 1.1em;letter-spacing: 2px;line-height: 18px;margin-top:15px;">
             <h3>Serves: {{ recipe.Serves }}</h3>
-            <h3>Prep Time: {{ recipe.PrepTime }}</h3>
+            <h3>Total Time: {{ recipe.TotalTime }}</h3>
             </div>
             
             <hr />
     
             <div>
             <h1 class="heading" style="font-size: 1.7em;">Ingredients</h1>
-            <ul class="ingredients-list">
+            
+            <p v-if="typeof(recipe.Ingredients) == 'string'"></p>
+            <ul v-else class="ingredients-list">
                 <li v-for="ingredient in recipe.Ingredients">{{ ingredient }}</li>
             </ul>
             </div>
@@ -35,15 +37,17 @@ var RecipeModal = {
         </div>
     
         <div>
-            <img :src=" '../static/assets/recipes/' + recipe.ImageSrc" />
+            <img :src="recipe.ImageSrc" />
         </div>
         </div>
         
         <div class="directions">
-        <h1 class="heading" style="font-size: 2em;">Directions:</h1>
-        <ul>
-            <li v-for="instruction in recipe.Instructions">{{ instruction }}</li>
-        </ul>
+            <h1 class="heading" style="font-size: 2em;">Directions:</h1>
+
+            <p v-if="typeof(recipe.Instructions) == 'string'">{{ recipe.Instructions }}</p>
+            <ul v-else>
+                <li v-for="instruction in recipe.Instructions">{{ instruction }}</li>
+            </ul>
         </div>
     
     </div>
