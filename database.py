ca5200621e05cef95f8c8df4499ae4cf163f86c8
Ryan <hashtagpoop@gmail.com>
2021-10-13 23:34:07 -0700

    updated database for postgresql params

--- database.py
+++ database.py
@@ -8,16 +8,20 @@ development = True if os.environ.get("DATABASE_URL") == None else False
 
 if seeding:
     SQLALCHEMY_DATABASE_URL = "sqlite:///../data/sql_app.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 elif development:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./data/sql_app.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 else:
     SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL").replace(
         "postgres", "postgresql"
     )
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
